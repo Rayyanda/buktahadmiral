0fdead8087a958bdca8dced47ed292c9b3f24259
Rayyanda <rayyanda5113@gmail.com>
2025-02-16 15:44:02 +0700

    firebaseConfig

--- src/lib/firebaseConfig.jsx
+++ src/lib/firebaseConfig.jsx
@@ -6,20 +6,20 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, on
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.REACT_APP_FIREBASE_MEASUREMENT_ID
-  // apiKey: "AIzaSyAWmuj2HcsnSQ7bEimxhZVusGH-Zs2sdms",
-  // authDomain: "admiral-a1e84.firebaseapp.com",
-  // projectId: "admiral-a1e84",
-  // storageBucket: "admiral-a1e84.firebasestorage.app",
-  // messagingSenderId: "557067775987",
-  // appId: "1:557067775987:web:c870d2e0a3eff217926c86",
-  // measurementId: "G-Y6VXT6TSSK"
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAWmuj2HcsnSQ7bEimxhZVusGH-Zs2sdms",
+  authDomain: "admiral-a1e84.firebaseapp.com",
+  projectId: "admiral-a1e84",
+  storageBucket: "admiral-a1e84.firebasestorage.app",
+  messagingSenderId: "557067775987",
+  appId: "1:557067775987:web:c870d2e0a3eff217926c86",
+  measurementId: "G-Y6VXT6TSSK"
     
   };
 
